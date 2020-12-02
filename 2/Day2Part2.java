e14863561c2af629811a86be99ff9495c1dd16f8
Arturo <45513197+Calisto184AC0@users.noreply.github.com>
2020-12-02 09:47:55 +0100

    Day 2 Completed

---
+++ 2/Day2Part2.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * day2
+ */
+public class Day2Part2 {
+    
+    static class Line {
+    
+        int low, hight;
+        String letter, password;
+
+        public Line(int low, int hight, String letter, String password) {
+            this.low = low;
+            this.hight = hight;
+            this.letter = letter;
+            this.password = password;
+        }
+
+        public boolean isValid() {
+            boolean res = false;
+            if (password.contains(letter)) {
+                try {
+                    if ((password.charAt(low - 1) == letter.charAt(0) && password.charAt(hight - 1) != letter.charAt(0)) 
+                        || (password.charAt(low - 1) != letter.charAt(0) && password.charAt(hight - 1) == letter.charAt(0))) {
+                        res = true;
+                    }
+                } catch (StringIndexOutOfBoundsException e) {}
+            }
+
+            return res;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String file = "2/input";
+        BufferedReader br = null;
+
+        br = new BufferedReader(new FileReader(file));
+
+        String line;
+        List<Line> lines = new ArrayList<Line>();
+        String[] aux = br.readLine().split("[^0-9a-z]+");
+        lines.add(new Line(Integer.parseInt(aux[0]), Integer.parseInt(aux[1]), aux[2], aux[3]));
+        while ((line = br.readLine()) != null) {
+            aux = line.split("[^0-9a-z]+");
+            lines.add(new Line(Integer.parseInt(aux[0]), Integer.parseInt(aux[1]), aux[2], aux[3]));
+        }
+
+        int cont = 0;
+        for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).isValid()) { cont++; }
+        }
+
+        System.out.println(cont);
+
+        br.close();
+    }
+}
